155483800e75f768acfc74a1c15cea60a8822cb4
Sam <samleo0823@gmail.com>
2020-06-10 16:09:15 -0500

    school search displays to page

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { SchoolapiService } from 'src/app/services/schoolapi.service';
-import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-search',
@@ -10,17 +9,28 @@ import { EventEmitter } from 'protractor';
 export class SearchComponent implements OnInit {
 
   public searchData: any;
+  public stateCode: string;
   public zipcode: string;
 
   constructor(private _schoolapiservice: SchoolapiService) { }
 
-  @Output() searchClicked = new EventEmitter();
-
   ngOnInit(): void {
+
+    
   }
 
   search(){
     console.log(this.zipcode);
+    console.log(this.stateCode);
+    this._schoolapiservice
+    .getSchoolsByState(this.stateCode, this.zipcode)
+    .subscribe(response => {
+      this.searchData = response;
+      console.log(this.searchData);
+    },
+    error => {
+      console.log(error);
+    })
     // this.searchClicked.emit(this.zipcode);
   }
 
