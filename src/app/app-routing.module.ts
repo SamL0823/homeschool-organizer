922160be1ad29a60044bafec1de30015df31f1f2
Sam <samleo0823@gmail.com>
2020-06-20 14:26:30 -0500

    unterminated string literal in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
-import { UserinfoComponent } from './pages/userinfo/userinfo.component
+import { UserinfoComponent } from './pages/userinfo/userinfo.component';
 import { ChildinfoComponent } from './pages/childinfo/childinfo.component';
 import { SearchComponent } from './pages/search/search.component';
 
